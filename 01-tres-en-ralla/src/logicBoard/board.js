c457e69dfc0ebb1314bbf400ddb7eb6082bba338
Bau_ <basach1582@gmail.com>
2025-10-26 17:13:52 -0300

    cree funciones para actualizar el tablero y verificar el ganador en el juego

---
+++ 01-tres-en-ralla/src/logicBoard/board.js
@@ -0,0 +1,21 @@
+import { TURNS } from "../constants"
+import { checkWinner, checkEndGame } from "./game"
+
+export const updateBoard = (index, board, setBoard, turn, setTurn, winner, setWinner) => {
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    console.log(newBoard)
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
